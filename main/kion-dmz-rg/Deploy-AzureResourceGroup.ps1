19833027952428b99162330cba74b6a3a6350178
Phuong <minhphuong90@gmail.com>
2018-02-06 17:51:51 +0700

    ok

--- main/kion-dmz-rg/Deploy-AzureResourceGroup.ps1
+++ main/kion-dmz-rg/Deploy-AzureResourceGroup.ps1
@@ -3,9 +3,12 @@ $location = "eastus"
 $templateFile = "azuredeploy.json"
 $templateParameterFile = "azuredeploy.parameters.json"
 
+Remove-AzureRmResourceGroup -Name $resourceGroup
+
 #Login-AzureRmAccount 
 
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location -Force
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $templateParameterFile
+cls
 
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $templateFile -TemplateParameterFile $templateParameterFile
